c59ccd5f4de216b66a468b9b4ba95ec92ce51c82
Jeremy Loulou <jeremy@jloulou.com>
2024-10-09 13:15:29 -0400

    Added sorting by note length.

--- sort.go
+++ sort.go
@@ -15,6 +15,10 @@ func sortNotesMut(n []note, mode string) {
 		sort.Slice(n, func(i, j int) bool {return n[i].created > n[j].created})
 	case "created-old":
 		sort.Slice(n, func(i, j int) bool {return n[i].created < n[j].created})
+	case "size-s":
+		sort.Slice(n, func(i, j int) bool {return len(n[i].body) < len(n[j].body)})
+	case "size-l":
+		sort.Slice(n, func(i, j int) bool {return len(n[i].body) > len(n[j].body)})
 	default:
 		return
 	}
